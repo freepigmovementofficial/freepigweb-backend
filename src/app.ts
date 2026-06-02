5e3c8925f20fbbe134098e90a38ab4d55f4b480e
Beli Dermawan <belidermawan01@gmail.com>
2026-06-02 08:30:20 +0700

    fix: configure helmet to allow cross-origin resource policy

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Security
-app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(
   cors({
     origin: (origin, callback) => {
